98db1f23aa4ae66b8b2939fcb07d588b64a3fcf8
Grant <Grant@DESKTOP-QLR0NAA>
2016-12-10 17:01:04 -0600

    Updated to use input buffer. Run command used to get this to execute after export, "java -cp C:\Users\Grant\Desktop\RunnableExportHelloWorld\RunnableBuild1.jar businessLayer.Program"

--- src/businessLayer/Program.java
+++ src/businessLayer/Program.java
@@ -1,13 +1,26 @@
 package businessLayer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//System.out.println("Hello World");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter String");
+		String s = br.readLine();
+		System.out.print("Enter Integer");
+		try{
+			int i = Integer.parseInt(br.readLine());
+		}catch(NumberFormatException ex){
+			System.err.println("Invalid Format!");
+		}
 		
 		
-	    System.out.println("Hello World");
-	 
+		
 	    
 	}
 
